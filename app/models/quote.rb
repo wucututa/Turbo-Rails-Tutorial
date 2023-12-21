d13ad495a071f4537d9d7ede97312c623faf546e
Do Ngoc Minh <minhdn@rikkeisoft.com>
2023-12-21 14:13:08 +0700

    Chapter 5 Real-time updates with Turbo Streams

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,15 @@
 class Quote < ApplicationRecord
     validates :name, presence: true
     scope :ordered, -> { order(id: :desc) }
+    # after_create_commit -> { broadcast_prepend_to "quotes" }
+    # after_update_commit -> { broadcast_replace_to "quotes" }
+    # after_destroy_commit -> { broadcast_remove_to "quotes" }
+
+    # Making broadcasting asynchronous
+    # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+    # after_update_commit -> { broadcast_replace_later_to "quotes" }
+    # The "broadcast_remove_later_to" method does not exist because as the quote gets deleted from the database
+
+    # syntactic sugar    
+    broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 end
